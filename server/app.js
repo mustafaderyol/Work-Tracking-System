7aa900c5b0e33e792c03773f361cc92b697ef16e
Mustafa DERYOL <mustafa.deryol@hotmail.com>
2018-11-03 19:39:17 +0200

    cross platform
    cross platform

--- server/app.js
+++ server/app.js
@@ -34,6 +34,13 @@ app.use('/api/users', usersRouter);
 app.use('/api/works', worksRouter);
 app.use('/api/userworkpivots', userWorkPivotsRouterRouter);
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers','Content-Type');
+    next();
+});
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
     next(createError(404));
